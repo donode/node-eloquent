88166afbc6b12e2be2900bd62b3b49ae50ed9f56
madhusudhand <madhusudhan.dollu@gmail.com>
2017-08-19 18:24:38 +0530

    adding transaction support

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+const util = require('util');
 const Eloquent = require('./lib/eloquent');
+const transaction = require('./lib/transaction');
 const config = require('./lib/config');
+const connection = require('./lib/connection');
 
 module.exports = {
   Eloquent,
   config: config.setConfig,
+  connect,
+  transaction,
 };
+
+function connect() {
+  connection.getPool(config.getConfig());
+}
